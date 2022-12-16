a5556a3d15139387df6f4c253f52a71cb666e74b
Marci-420 <meszaros.marcell.zsolt.2020i@bankitatabanya.hu>
2022-12-16 12:37:17 +0100

    kettedik és háromadik feladat
    utsó nincs meg :-1:

--- 2022-12-14/js.js
+++ 2022-12-14/js.js
@@ -7,20 +7,34 @@ function dereke(){
         alert("jooooooo ez derékszög")
     }
     else {
-        alert("gyóó")
+        alert("jájj ez nem derékszögűű")
     }
 }
 
-function meterTo {
-    var m = parseInt(document.getElementById("m").value)
-    
+function meterTo() {
+    var m = document.getElementById("m").value;
+
     let ft = m *3.2808398950131
     let mile = m * 1609.344
     let km = m* 1000
     let y = m * 1.0936133
     let inch = m*39.3700787
 
-    document.getElementById().value = ft + "láb:"+mile + "mérföld:"+ km + "kilométer:" + y + "yard:" + inch + "inch"
+    document.getElementById("ce").innerHTML = ft +" láb:";
+    document.getElementById("cE").innerHTML = mile + " mérföld";
+    document.getElementById("Ce").innerHTML = km + " kilométer";
+    document.getElementById("CE").innerHTML = y + " yard";
+    document.getElementById("ci").innerHTML = inch + " inch";
+}
 
+function secTo(){
+    let sec = document.getElementById("sec").value;
+
+    let h = Math.floor(sec / 3600);
+    sec %= 3600;
+    let min = Math.floor(sec / 60);
+    let secs = sec % 60;
+
+    document.getElementById("c").innerHTML = "Idő:"+h + ":" + min + ":" + secs;
 }
        
